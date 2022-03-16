69a9bf540f8bfe96a68b6aaceeec415802d29a4b
RishabhHarman04 <er.23rishabh@gmail.com>
2022-03-16 12:59:53 +0530

    some common proframs

---
+++ main.py
@@ -0,0 +1,56 @@
+# join method
+
+myTuple = ("John", "Peter", "Vicky")
+
+x = "rishabh".join(myTuple)
+
+print(x)
+
+
+
+
+
+#split method
+
+txt = "welcome to the jungle"
+
+x = txt.split()
+
+print(x)
+
+
+
+
+
+# format method
+
+str = "This article is written in {}"
+
+print(str.format("Python"))
+
+
+
+
+
+# replace method
+
+txt = "I like bananas"
+
+x = txt.replace("bananas", "apples")
+
+print(x)
+
+
+
+
+
+# strip method
+
+string="     rishabh        "
+
+print(string.strip())
+
+
+
+
+
